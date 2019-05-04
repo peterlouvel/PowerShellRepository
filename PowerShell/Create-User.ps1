804e6bd0982dfef4344c68f35c6f0cd68a290a41
Peter Louvel <peterlouvel@gmail.com>
2019-05-04 21:53:09 +1000

    comments added

---
+++ PowerShell/Create-User.ps1
@@ -0,0 +1,32 @@
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.EXAMPLE
+    PS C:\> <example usage>
+    Explanation of what the example does
+.INPUTS
+    Inputs (if any)
+.OUTPUTS
+    Output (if any)
+.NOTES
+    General notes
+#>
+
+function Verb-Noun {
+    [CmdletBinding()]
+    param (
+        
+    )
+    
+    begin {
+    }
+    
+    process {
+    }
+    
+    end {
+    }
+}
+
