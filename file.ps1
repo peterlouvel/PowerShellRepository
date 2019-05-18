10e88d82e1b7f654237addb655d692039fe6cd79
Peter Louvel <peterlouvel@gmail.com>
2019-05-18 12:14:50 +1000

    update

--- file.ps1
+++ file.ps1
@@ -1,31 +1,31 @@
 <# This form was created using POSHGUI.com  a free online gui designer for PowerShell
 .NAME
-    Untitled
+    Get Service Info
 #>
 
+# Functions are at the top so the script engine won't complain about not knowing a funciton.
+function MyFunction1 {
+    
+    # Write-Host "Selected: " $LBoxPick.Text
+    #.Text = $LBoxPick.Text
+}
+
+#region begin GUI{ 
+Add-Type -AssemblyName System.Drawing
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.Application]::EnableVisualStyles()
 
-#region begin GUI{ 
-  
 $Form                            = New-Object system.Windows.Forms.Form
 $Form.ClientSize                 = '400,400'
 $Form.text                       = "Service Inspector"
 $Form.TopMost                    = $false
-
-$LBoxPick                        = New-Object System.Windows.Forms.ComboBox
-
-$LBoxPick.width                  = 318
-$LBoxPick.height                 = 30
-$LBoxPick.location               = New-Object System.Drawing.Point(24,62)
-
 
 $LblPick                         = New-Object system.Windows.Forms.Label
 $LblPick.text                    = "Pick Service"
 $LblPick.AutoSize                = $true
 $LblPick.width                   = 25
 $LblPick.height                  = 10
-$LblPick.location                = New-Object System.Drawing.Point(30,34)
+$LblPick.location                = '30,34'
 $LblPick.Font                    = 'Microsoft Sans Serif,10'
 
 # $BtnCheckSvc                     = New-Object system.Windows.Forms.Button
@@ -36,13 +36,12 @@ $LblPick.Font                    = 'Microsoft Sans Serif,10'
 # $BtnCheckSvc.Font                = 'Microsoft Sans Serif,10'
 
 $LblShow                         = New-Object system.Windows.Forms.Label
-$LblShow.text                    = ""
+# $LblShow.text                    = ""
 $LblShow.AutoSize                = $true
 $LblShow.width                   = 25
 $LblShow.height                  = 10
 $LblShow.location                = '27,176'
 $LblShow.Font                    = 'Microsoft Sans Serif,10'
-
 
 $TboxStatus                      = New-Object system.Windows.Forms.TextBox
 $TboxStatus.multiline            = $false
@@ -56,34 +55,21 @@ $LblCurrent.text                 = "Curent Status"
 $LblCurrent.AutoSize             = $true
 $LblCurrent.width                = 25
 $LblCurrent.height               = 10
-$LblCurrent.location             = New-Object System.Drawing.Point(30,153)
+$LblCurrent.location             = '30,153'
 $LblCurrent.Font                 = 'Microsoft Sans Serif,10'
 
-$Form.controls.AddRange(@($LBoxPick,$LblPick,$BtnCheckSvc,$TboxStatus,$LblCurrent))
+$LBoxPick                        = New-Object System.Windows.Forms.ListBox
+$LBoxPick.width                  = 318
+$LBoxPick.height                 = 100
+$LBoxPick.location               = '24,62'
 
-#region gui events {
-#$BtnCheckSvc.Add_Click({ Click_status_button })
-$LBoxPick. ({ Click_SelectBox })
-
-
-
-#endregion events }
+$Form.controls.AddRange(@($LBoxPick,$LblPick,$BtnCheckSvc,$TboxStatus,$LblCurrent,$LblShow))
 
 #endregion GUI }
+
+$LBoxPick.Add_SelectedValueChanged({ MyFunction1 $this $_  })
+
 $Servs = Get-Service | Select-Object -Property name
 $LBoxPick.Items.AddRange($Servs.name)
-$LBoxPick.Text = $Servs[0].name
-
-#Write your logic code here
- 
-function Click_status_button {
-       
-}
-
-function Click_SelectBox {
-    
-    Write-Host "Selected: " $LBoxPick.Text
-     
- }
 
 [void]$Form.ShowDialog()
