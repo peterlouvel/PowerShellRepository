a271a2cd69a7ae446ca3162d448615f95a8edeef
Peter Louvel <peter.louvel@edmi.com.au>
2020-05-13 08:58:17 +1000

    o365 email done

--- Enable-O365Email.ps1
+++ Enable-O365Email.ps1
@@ -14,8 +14,8 @@
     Domain can be
         au
         nz
-
-        AccountSkuID	            Description
+    https://docs.microsoft.com/en-us/azure/active-directory/users-groups-roles/licensing-service-plan-reference
+      AccountSkuID	            Description
         BI_AZURE_P1	                Power BI (Business Intelligence) Reporting and Analytics
         CRMIUR	                    Customer Relationship Management Internal Use Rights
         DESKLESSPACK	            Office 365 (Plan K1)
@@ -23,7 +23,7 @@
         DEVELOPERPACK	            Office 365 For Developers
         ECAL_SERVICES	            Enterprise Client Access Services
         EMS	                        Enterprise Mobility Suite
-        ENTERPRISEPACK	            Enterprise Plan E3
+        ENTERPRISEPACK	            Enterprise Plan E3      6fd2c87f-b296-42f0-b197-1e91e994b900
         ENTERPRISEPACK_B_PILOT	    Office 365 (Enterprise Preview)
         ENTERPRISEPACK_FACULTY	    Office 365 (Plan A3) for Faculty
         ENTERPRISEPACK_STUDENT	    Office 365 (Plan A3) for Students
@@ -48,7 +48,7 @@
         RIGHTSMANAGEMENT_ADHOC	    Windows Azure Rights Management
         SHAREPOINTSTORAGE	        SharePoint storage
         STANDARD_B_PILOT	        Office 365 (Small Business Preview)
-        STANDARDPACK	            Enterprise Plan E1
+        STANDARDPACK	            Enterprise Plan E1      18181a46-0d4e-45cd-891e-60aabd171b4e
         STANDARDPACK_FACULTY	    Office 365 (Plan A1) for Faculty
         STANDARDPACK_STUDENT	    Office 365 (Plan A1) for Students
         STANDARDWOFFPACK	        Office 365 (Plan E2)
@@ -59,7 +59,7 @@
         Get-MsolSubscription | Where-Object {($_.Status -ne "Suspended")}
 #>
 
-
+#Black, DarkBlue, DarkGreen, DarkCyan, DarkRed, DarkMagenta, DarkYellow, Gray, DarkGray, Blue, Green, Cyan, Red, Magenta, Yellow, White
 
 param(
     [Parameter(Mandatory=$true)]
@@ -73,31 +73,6 @@ param(
 )
 
 [String] ${stUserDomain},[String]  ${stUserAccount} = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.split("\")
-$AdminAccount = $stUserAccount + "_"
-
-if ($Domain -eq "au"){
-    $End = "@edmi.com.au"
-    $DomainController = "AuBneDC11.au.edmi.local"
-    $AdminAccount1 = "au\"+$AdminAccount
-    if ($null -eq $Cred){
-        $Cred = Get-Credential $AdminAccount1
-    } 
-} elseif ($Domain -eq "nz"){
-    $End = "@edmi.co.nz"
-    # $DomainController = "NZwlgDC3.nz.edmi.local"
-    $DomainController = "NzBneDC5.nz.edmi.local"
-    $AdminAccount1 = "nz\"+$AdminAccount
-    if ($null -eq $Cred){
-        $Cred = Get-Credential $AdminAccount1
-    }
-} else {
-    Write-Host "Domain should be AU or NZ"
-    exit
-}
-
-Write-Host
-Write-Host "Setup your Credentials for accessing the local exchange server" -ForegroundColor Cyan  
-Write-Host $AdminAccount1
 
 if ($stUserDomain -eq "au"){
     $EndAdmin = "@edmi.com.au"
@@ -108,46 +83,68 @@ if ($stUserDomain -eq "au"){
     exit
 }
 
+if ($Domain -eq "au"){
+    $End = "@edmi.com.au"
+    $DomainController = "AuBneDC11.au.edmi.local"
+    if ($null -eq $Cred){
+        $AdminAccount = $stUserAccount + "_"
+        $AdminAccount1 = "au\"+$AdminAccount
+        $Cred = Get-Credential $AdminAccount1
+    } 
+} elseif ($Domain -eq "nz"){
+    $End = "@edmi.co.nz"
+    # $DomainController = "NZwlgDC3.nz.edmi.local"
+    $DomainController = "NzBneDC5.nz.edmi.local"
+    if ($null -eq $Cred){
+        $AdminAccount = $stUserAccount + "_"
+        $AdminAccount1 = "nz\"+$AdminAccount
+        $Cred = Get-Credential $AdminAccount1
+    }
+} else {
+    Write-Host "Domain should be AU or NZ" -ForegroundColor Red  
+    exit
+}
+
 $UserLowerCase = $User.ToLower()
 $Email = $UserLowerCase + $End
 # Write-Host "User eMail:  $Email"
 Write-Host
-Write-Host "Setup your Credentials for accessing the Office 365 systems" -ForegroundColor Cyan  
+Write-Host "Setup your Credentials for accessing the Office 365 systems - " -ForegroundColor Green  -NoNewline
 $Account = $stUserAccount + $EndAdmin
-Write-Host $Account
-if ($null -eq $O365CREDS){
-    $O365CREDS = Get-Credential $Account
-} 
+Write-Host $Account 
+$temp = Install-Module -Name AzureAD
+$temp = Connect-AzureAD -AccountId $Account
 
-# Install-Module -Name AzureAD
-# Connect-AzureAD -AccountId $Email
-# Get-AzureADUser -ObjectId
-
-
-Write-Host
-Write-Host "Connecting to Office 365" -ForegroundColor Green  
-Connect-MSOLService -Credential $O365CREDS
-If (Get-MsolUser -UserPrincipalName $Email -erroraction 'silentlycontinue') {
-    Write-Host "User $Email Exits. Creating Mailbox on O365" -ForegroundColor Cyan  
-} else {
-    Write-Host "User $Email doesn't exist, wait till the user is on O365" -ForegroundColor White  
+try {
+    $temp = Get-AzureADUser -ObjectId $Email #-erroraction 'silentlycontinue'
+}
+catch {
+    Write-Host "User" -ForegroundColor Red  -NoNewline
+    Write-Host " $User " -ForegroundColor Cyan  -NoNewline
+    Write-Host "doesn't exist on Office 365 yet - wait till the user is on O365" -ForegroundColor Red  
     exit
 }
+
+Write-Host "User " -ForegroundColor Green  -NoNewline
+Write-Host " $User " -ForegroundColor Cyan -NoNewline 
+Write-Host " exits. Creating Mailbox on O365" -ForegroundColor Green 
 Write-Host
 Write-Host "Connecting to the Local Exchange Server" -ForegroundColor Green  
 # Create user local AD, sync AD to O365 then when synced, run the following
-$Session1 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://edmibneexch1.edmi.local/powershell -Credential $Cred
-Import-PSSession $Session1 3>$null
+$Session1 = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri http://edmibneexch3.edmi.local/powershell -Credential $Cred
+$temp = Import-PSSession $Session1 3>$null
 $UserO365email = $UserLowerCase + "@edmi.mail.onmicrosoft.com"
-Write-Host 
-Write-Host "Setting up $UserLowerCase remote mailbox" -ForegroundColor Green  
-Enable-RemoteMailbox -Identity $UserLowerCase  -DomainController $DomainController -RemoteRoutingAddress $UserO365email
+Write-Host "Setting up remote mailbox for user " -ForegroundColor Green -NoNewline
+Write-Host " $UserLowerCase " -ForegroundColor Cyan  
+Write-Host
+
+$temp = Enable-RemoteMailbox -Identity $UserLowerCase  -DomainController $DomainController -RemoteRoutingAddress $UserO365email -erroraction 'silentlycontinue'
+
 Exit-PSSession
 Remove-PSSession $Session1
 
 Write-Host "------------------------------------------------------------------------------------------------"
 Write-Host "Waiting a couple minutes for O365 email account to be created before enabling licence." -ForegroundColor Cyan  
-Write-Host "This just seems to work" -ForegroundColor Green  
 Write-Host "------------------------------------------------------------------------------------------------"
 Start-Sleep -s 15
 Write-Host "----- 0:15"
@@ -166,12 +163,25 @@ Write-Host "----- 1:45"
 Start-Sleep -s 15
 Write-Host "----- 2:00"
 
-# Give E3 licence to user
-Connect-MSOLService -Credential $O365CREDS
-Get-ADUser $UserLowerCase | Set-MsolUser  -UsageLocation $Domain                   # Sets the location (Country) of the user
+# Give licence to user
+
+Set-AzureADUser -ObjectId $Email -UsageLocation $Domain
+#Get-ADUser $UserLowerCase -Credential $Cred -Server $DomainController | Set-MsolUser  -UsageLocation $Domain                   # Sets the location (Country) of the user
+# Set-AzureADUser -ObjectId $UserLowerCase.UserPrincipleName -City " "
 If ($LicenceCode -eq "E3") {
-    Set-MsolUserLicense -UserPrincipalName $Email -AddLicenses "EDMI:ENTERPRISEPACK"   # Gives E3 licence
+    $planName="ENTERPRISEPACK"
 }
 If ($LicenceCode -eq "E1") {
-    Set-MsolUserLicense -UserPrincipalName $Email -AddLicenses "EDMI:STANDARDPACK"   # Gives E3 licence
+    $planName="STANDARDPACK"
 }
+Write-Host "Give licence " -ForegroundColor Green -NoNewline
+Write-Host " $planName " -ForegroundColor Cyan -NoNewline
+Write-Host " to user " -ForegroundColor Green -NoNewline
+Write-Host " $User" -ForegroundColor Cyan
+
+$License = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicense
+$License.SkuId = (Get-AzureADSubscribedSku | Where-Object -Property SkuPartNumber -Value $planName -EQ).SkuID
+$LicensesToAssign = New-Object -TypeName Microsoft.Open.AzureAD.Model.AssignedLicenses
+$LicensesToAssign.AddLicenses = $License
+Set-AzureADUserLicense -ObjectId $Email -AssignedLicenses $LicensesToAssign
+Write-Host
