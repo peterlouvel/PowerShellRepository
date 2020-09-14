a0d80aa085f945a4dbd35c3521d346e3772541e9
Peter Louvel <peter.louvel@edmi.com.au>
2020-09-15 09:05:25 +1000

    more info

--- get-UserExpiry.ps1
+++ get-UserExpiry.ps1
@@ -14,7 +14,8 @@
     .OUTPUTS
         A user/s name and password expiry date
     .NOTES
-
+    default domain is au
+    you can use sg, nz, uk domains
 #>
 
 param(
@@ -22,22 +23,26 @@ param(
     [string]$UsersAccount
     ,
     [Parameter(Mandatory=$False)]
-    [string]$UsersDomain
+    [string]$UsersDomain="au"
 )
 
 if ($UsersAccount) {
     try {
-        Get-ADUser "$UsersAccount" -Server $UsersDomain -Properties "Name", "LastLogonDate", "msDS-UserPasswordExpiryTimeComputed" | 
-        Select-Object -Property "Name", "LastLogonDate", @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | 
-        sort -Property Name
+        Get-ADUser "$UsersAccount" -Server $UsersDomain -Properties "Name", "pwdLastSet", "LastLogonDate", "LastLogon", "msDS-UserPasswordExpiryTimeComputed" | 
+        Select-Object -Property "Name", "LastLogonDate", @{Name="LastLogon";Expression={[datetime]::FromFileTime($_."LastLogon")}}, @{Name="PWD Last Changed";Expression={[datetime]::FromFileTime($_."pwdLastSet")}}, @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} |
+        format-table 
     } catch {
-        Write-Host "User " -ForegroundColor  Green -NoNewline
-        Write-Host "$UsersAccount " -ForegroundColor  Red -NoNewline
-        write-host "does not exist" -ForegroundColor  Green
+        Write-Host "User " -ForegroundColor Green -NoNewline
+        Write-Host "$UsersAccount " -ForegroundColor Red -NoNewline
+        write-host "does not exist" -ForegroundColor Green
     }
 } else {
-    Get-ADUser  -Server $UsersDomain -filter {(Enabled -eq $True) -and (PasswordNeverExpires -eq $False)} -Properties "Name", "LastLogonDate",  "msDS-UserPasswordExpiryTimeComputed" | 
-    Select-Object -Property "Name", "LastLogonDate" , @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | 
-    sort -Property Name
+    Get-ADUser -Server $UsersDomain -filter {(Enabled -eq $True) -and (PasswordNeverExpires -eq $False)} -Properties "Name", "pwdLastSet", "LastLogonDate", "LastLogon",  "msDS-UserPasswordExpiryTimeComputed" | 
+    Select-Object -Property "Name", "LastLogonDate", @{Name="LastLogon";Expression={[datetime]::FromFileTime($_."LastLogon")}}, @{Name="PWD Last Changed";Expression={[datetime]::FromFileTime($_."pwdLastSet")}}, @{Name="ExpiryDate";Expression={[datetime]::FromFileTime($_."msDS-UserPasswordExpiryTimeComputed")}} | 
+    sort -Property Name |
+    format-table 
 }
 
+Write-Host "There is a difference between LastLogonDate and LastLogon"
+Write-Host "Read artical to find out more"
+Write-Host "https://community.spiceworks.com/topic/2062846-lastlogon-lastlogondate-and-lastlogontimestamp"
