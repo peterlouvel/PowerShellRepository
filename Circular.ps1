57a222dab92e316719b5fd45e85f75c0a531e211
Peter Louvel <peterlouvel@gmail.com>
2021-09-30 08:55:19 +1000

    update circular

--- Circular.ps1
+++ Circular.ps1
@@ -1,4 +1,3 @@
-# $ErrorActionPreference = 'SilentlyContinue'
 # Circular.ps1
 # List all AD groups that contain one or more circular nested groups
 # outputs the parent group's DN and a list of the nested groups.
@@ -19,18 +18,20 @@
 
 cls
 # Import-Module Activedirectory
-
+$outFile = 'c:\temp\CircularCheck.txt'
+if (!(Test-Path "c:\temp")){New-Item -Path "c:\temp" -ItemType Directory | Out-Null}
+if (Test-Path $outFile) {Remove-Item $outFile}
+New-Item -Path $outFile -ItemType File | Out-Null
+Add-Content -Path $outFile -Value (Get-Date) 
 write-host "Circular.ps1  Search for nested groups - getting Groups"
 # Retrieve all top/parent level AD groups.
 $Parents = get-adgroup -ResultPageSize 1000 -filter 'ObjectClass -eq "group"' -Server "edmi.local"
-
 # Loop through each parent group
-ForEach ($Parent in $Parents) {
+ForEach ($Parent in $Parents) { 
    [int]$Len = 0
    # Create an array of the group members, limited to sub-groups (not users)
    $Children = @(Get-ADGroupMember "$Parent" -Server "edmi.local" | where {$_.objectClass -eq "group"} )
    $Len = @($Children).Count
-
    if ($Len -eq 1) {
       Write-host "$Parent" -ForegroundColor White -NoNewline
       write-host " contains 1 group " -ForegroundColor Cyan -NoNewline
@@ -39,7 +40,6 @@ ForEach ($Parent in $Parents) {
       Write-host "$Parent" -ForegroundColor White -NoNewline
       write-host " contains $Len groups " -ForegroundColor Cyan -NoNewline
    }
-   
    if ($Len -gt 0) {
       write-host "--check nesting" -ForegroundColor Green
       ForEach ($Child in $Children) {
@@ -52,14 +52,8 @@ ForEach ($Parent in $Parents) {
             write-host "   Fopund a circular nested group:" -ForegroundColor Red -NoNewline 
             write-host " $nested" -ForegroundColor Cyan -NoNewline 
             write-host " is both a parent and a member of " -ForegroundColor Red -NoNewline 
+            Add-Content -Path $outFile -Value "$nested"
          }
-
-         # if ($Child.Contains("DC=au")){$Server = "au.edmi.local"
-         # }elseif ($Child.Contains("DC=nz")){$Server = "nz.edmi.local"
-         # }elseif ($Child.Contains("DC=uk")){$Server = "uk.edmi.local"
-         # }elseif ($Child.Contains("DC=sg")){$Server = "sg.edmi.local"
-         # }else{$Server = "edmi.local"}
-
          write-host "---check further nesting" -ForegroundColor Green -NoNewline 
          ForEach ($Parent1 in $Child) {
             $Server = "edmi.local"
@@ -67,26 +61,24 @@ ForEach ($Parent in $Parents) {
             if ($Parent1 -like "*DC=nz*"){$Server = "nz.edmi.local"}
             if ($Parent1 -like "*DC=uk*"){$Server = "uk.edmi.local"}
             if ($Parent1 -like "*DC=sg*"){$Server = "sg.edmi.local"}
-
             [int]$Len1 = 0
             # Create an array of the group members, limited to sub-groups (not users)       
             $Children1 = @(Get-ADGroupMember "$Parent1" -Server "$Server" | where {$_.objectClass -eq "group"}) 
             $Len1 = @($Children1).Count
-
             if ($Len1 -eq 1) {write-host " contains 1 group " -ForegroundColor Cyan -NoNewline}
             elseif ($Len1 -gt 0) {write-host " contains $Len1 groups " -ForegroundColor Cyan -NoNewline}
-            
             if ($Len1 -gt 0) {
                write-host "--check nesting" -ForegroundColor DarkYellow
                ForEach ($Child1 in $Children1) {
-                  # Now find any member of $Child1 which is also the $Parent
+                  # Now find any member of $Child1 is also a member of the $Parent 
                   $nested1 = @(Get-ADGroupMember $Child1 | where {$_.objectClass -eq "group" -and "$_" -eq "$Parent"} )
                   $NestCount1 = @($nested1).Count
                   write-host "- $Child1 " -ForegroundColor DarkYellow
                   if ($NestCount1 -gt 0) {
                      write-host "  Found a circular nested group:" -ForegroundColor DarkRed -NoNewline 
-                     write-host " $nested" -ForegroundColor Cyan
+                     write-host " $nested1" -ForegroundColor Cyan
                      write-host " is both a parent and a member of" -ForegroundColor DarkRed
+                     Add-Content -Path $outFile -Value "$nested1"
                   }
                }
             #write-host "--done" -ForegroundColor Green
--- Find-Circular.ps1
+++ Find-Circular.ps1
@@ -17,24 +17,18 @@ param(
     ,[Parameter(Mandatory=$false)]
     [string]$Server = "edmi.local"
 )
-
 cls
 # Import-Module Activedirectory
-
 [String] ${stYourDomain},[String]  ${stYourAccount} = [System.Security.Principal.WindowsIdentity]::GetCurrent().Name.split("\")
 $AdminAccount = $stYourAccount + "_"
-
-write-host "Circular.ps1  Search for nested groups - getting Groups"
 # Retrieve all top/parent level AD groups.
 $Parents = get-adgroup -ResultPageSize 1000 -filter 'ObjectClass -eq "group"' -Server "$Server" | Where-Object Name -eq "$Group"
-
 # Loop through each parent group
 ForEach ($Parent in $Parents) {
    [int]$Len = 0
    # Create an array of the group members, limited to sub-groups (not users)
    $Children = @(Get-ADGroupMember "$Parent" -Server "$Server" | where {$_.objectClass -eq "group"} )
    $Len = @($Children).Count
-
    if ($Len -eq 1) {
       Write-host "$Parent" -ForegroundColor White -NoNewline
       write-host " contains 1 group " -ForegroundColor Cyan -NoNewline
@@ -43,7 +37,6 @@ ForEach ($Parent in $Parents) {
       Write-host "$Parent" -ForegroundColor White -NoNewline
       write-host " contains $Len groups " -ForegroundColor Cyan -NoNewline
    }
-   
    if ($Len -gt 0) {
       write-host "--check nesting" -ForegroundColor Green
       ForEach ($Child in $Children) {
@@ -54,11 +47,8 @@ ForEach ($Parent in $Parents) {
          if ($NestCount -gt 0) {
             write-host ""
             write-host "   Fopund a circular nested group:" -ForegroundColor Red -NoNewline 
-            write-host " $nested" -ForegroundColor Cyan -NoNewline 
-            write-host " is both a parent and a member of " -ForegroundColor Red -NoNewline 
+            write-host " $nested" -ForegroundColor Cyan  
          }
-
-
          write-host "---check further nesting" -ForegroundColor Green 
          ForEach ($Parent1 in $Child) {
             # write-host "$Parent1"
@@ -67,9 +57,7 @@ ForEach ($Parent in $Parents) {
             if ($Parent1 -like "*DC=nz*"){$Server = "nz.edmi.local"}
             if ($Parent1 -like "*DC=uk*"){$Server = "uk.edmi.local"}
             if ($Parent1 -like "*DC=sg*"){$Server = "sg.edmi.local"}
-
             [int]$Len1 = 0
-            # write-host "$Server"
             # Create an array of the group members, limited to sub-groups (not users)       
             $Children1 = @(Get-ADGroupMember "$Parent1" -Server "$Server" | where {$_.objectClass -eq "group"}) 
             $Len1 = @($Children1).Count
@@ -86,15 +74,12 @@ ForEach ($Parent in $Parents) {
                   write-host "- $Child1 " -ForegroundColor DarkYellow
                   if ($NestCount1 -gt 0) {
                      write-host "  Found a circular nested group:" -ForegroundColor DarkRed -NoNewline 
-                     write-host " $nested" -ForegroundColor Cyan
-                     write-host " is both a parent and a member of" -ForegroundColor DarkRed
+                     write-host " $nested1" -ForegroundColor Cyan
                   }
                }
-            #write-host "--done" -ForegroundColor Green
             }else{write-host ""}
          }
       }
-   #write-host "--done" -ForegroundColor Green
    }
 }
 
